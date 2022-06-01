28dc58b9d46653812d29d2998393c31c470d1b9e
Jackson Clarke <104281508+XA1phaX@users.noreply.github.com>
2022-05-31 18:22:41 -0600

    prove08 and gitignore

---
+++ prove08.py
@@ -0,0 +1,25 @@
+from Lists.words import *
+import random
+
+# Intro message
+print("\nWelcome to the word guessing game!\n\n")
+
+# Setting up vairables
+secretWord = random.choice(words)
+letterCount = len(secretWord)
+startingLetter = secretWord[0]
+guess = ""
+guess_count = 0
+
+# Temporary help texts
+# print(f"\nThe secret word is {letterCount} letters long.\n")
+# print(f"The first letter of the secret word is {startingLetter}.\n")
+print(f"Your hint is: {'_ '*letterCount}")
+# Run the game
+while guess != secretWord:
+    guess = input("Guess a word: ")
+    guess_count += 1
+    if guess == secretWord:
+        print(f"\nYou guessed correctly! It took you {guess_count} tries.")
+    else:
+        print("\nSorry, that's not it.")
