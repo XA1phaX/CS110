2fa165f45802b451a94b8de6a629668af4188976
Jackson Clarke <104281508+XA1phaX@users.noreply.github.com>
2022-06-01 20:15:37 -0600

    prove08 finish

--- prove08Real.py
+++ prove08Real.py
@@ -7,15 +7,17 @@ print("\nWelcome to the word guessing game!\n\n")
 # Setting up vairables
 secretWord = random.choice(words)
 letterCount = len(secretWord)
+# startingLetter = secretWord[0]
 guess = ""
 guess_count = 0
 i = 0
 hint = []
 for letter in secretWord:
     hint.append("_")
-hintUpdated = []
-
-# Start
+hintUpdated = hint
+# Temporary help texts
+# print(f"\nThe secret word is {letterCount} letters long.\n")
+# print(f"The first letter of the secret word is {startingLetter}.\n")
 print(secretWord)
 print(f"Your hint is: " + " ".join(hint) + "\n")
 
@@ -24,27 +26,27 @@ while guess != secretWord:
     guess = input("Guess a word: ")
     guess = guess.lower()
     guess_count += 1
+    hintUpdated = []
+    for letter in secretWord:
+        hintUpdated.append("_")
     if guess == secretWord:
         print(
             f"You guessed correctly! The word was {secretWord}! It took you {guess_count} tries."
         )
     else:
         for letter in guess:
-            if i > letterCount:
-                hintUpdated.append(letter.lower())
-                i += 1
-            else:
+            if i < letterCount:
                 if letter in secretWord:
-
                     if letter == secretWord[i]:
-                        hintUpdated.append(letter.upper())
+                        hintUpdated[i] = letter.upper()
                         i += 1
                     else:
-                        hintUpdated.append(letter.lower())
+                        hintUpdated[i] = letter.lower()
                         i += 1
                 else:
-                    hintUpdated.append("_")
+                    hintUpdated[i] = "_"
                     i += 1
+            else:
+                hintUpdated.append(letter.lower())
         i = 0
         print(f"\nYour hint is: " + " ".join(hintUpdated) + "\n")
-        hintUpdated = []
